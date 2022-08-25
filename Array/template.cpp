02a1665943b777c893d5906a853145fe14c7a0cf
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-08-25 20:20:23 +0530

    Create template.cpp

---
+++ Array/template.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> getArray()
+{
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter the array:";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    return arr;
+}
+
+void printArray(vector<int> arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr;
+    arr = getArray();
+
+    cout << "Original array: " << endl;
+    printArray(arr, arr.size());
+
+    return 0;
+}
