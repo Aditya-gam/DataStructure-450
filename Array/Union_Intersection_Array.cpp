29af093a4450677dfe48c2d2d524da1d32150b09
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-08-26 23:07:48 +0530

    Added intersection method

--- Array/Union_Intersection_Array.cpp
+++ Array/Union_Intersection_Array.cpp
@@ -1,58 +1,58 @@
 /*################## Using Set   ##################*/
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
-using namespace std;
+// using namespace std;
 
-vector<int> getArray()
-{
-    int n;
-    cout << "Enter size of array: ";
-    cin >> n;
-    vector<int> arr(n);
-    cout << "Enter the array:";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+// vector<int> getArray()
+// {
+//     int n;
+//     cout << "Enter size of array: ";
+//     cin >> n;
+//     vector<int> arr(n);
+//     cout << "Enter the array:";
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
 
-    return arr;
-}
+//     return arr;
+// }
 
-void printArray(vector<int> arr, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        cout << arr[i] << " ";
-    }
+// void printArray(vector<int> arr, int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
 
-    cout << endl;
-}
+//     cout << endl;
+// }
 
-int unIon(vector<int> arr1, vector<int> arr2)
-{
-    set<int> st;
-    for (int i = 0; i < arr1.size(); i++)
-    {
-        st.insert(arr1[i]);
-    }
-    for (int i = 0; i < arr2.size(); i++)
-    {
-        st.insert(arr2[i]);
-    }
+// int unIon(vector<int> arr1, vector<int> arr2)
+// {
+//     set<int> st;
+//     for (int i = 0; i < arr1.size(); i++)
+//     {
+//         st.insert(arr1[i]);
+//     }
+//     for (int i = 0; i < arr2.size(); i++)
+//     {
+//         st.insert(arr2[i]);
+//     }
 
-    return st.size();
-}
+//     return st.size();
+// }
 
-int main()
-{
-    vector<int> arr1, arr2;
-    arr1 = getArray();
-    arr2 = getArray();
+// int main()
+// {
+//     vector<int> arr1, arr2;
+//     arr1 = getArray();
+//     arr2 = getArray();
 
-    cout << "Lenght of Union: " << unIon(arr1, arr2) << endl;
+//     cout << "Lenght of Union: " << unIon(arr1, arr2) << endl;
 
-    return 0;
-}
+//     return 0;
+// }
 
 /*################## Without Using Set   ##################*/
 #include <bits/stdc++.h>
@@ -110,6 +110,36 @@ int unIon(vector<int> arr1, vector<int> arr2)
     return ans;
 }
 
+int intersection(vector<int> arr1, vector<int> arr2)
+{
+    int i = 0, j = 0, k = 0;
+    vector<int> result(arr1.size() + arr2.size());
+
+    while (i < arr1.size() && j < arr2.size())
+    {
+        if (arr1[i] < arr2[i])
+            i++;
+        else if (arr1[i] > arr2[i])
+            j++;
+        else
+        {
+            if (k != 0 && arr1[i] == result[k - 1])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                result[k] = arr1[i];
+                i++;
+                j++;
+                k++;
+            }
+        }
+    }
+    return result.size();
+}
+
 int main()
 {
     vector<int> arr1, arr2;
@@ -117,6 +147,7 @@ int main()
     arr2 = getArray();
 
     cout << "Lenght of Union: " << unIon(arr1, arr2) << endl;
+    cout << "Lenght of Intersection: " << intersection(arr1, arr2) << endl;
 
     return 0;
 }
